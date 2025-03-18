11000a732ebd63c7649afa8d6a54a4226ffdded4
mygitname <james1859635@gmail.com>
2025-03-19 00:34:38 +0800

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import VueSetupExtend from 'vite-plugin-vue-setup-extend'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/assets/',
+  base: '/',
   build: {
     assetsDir: "assets"
   },
